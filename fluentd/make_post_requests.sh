2100031a67e0cb992e84960acd0a955c4be5ff9e
Roman Orlovskyi <Roman_Orlovskyi@epam.com>
2019-07-01 15:44:01 +0300

    Implemented and tested basic container logging with fluentd

--- fluentd/make_post_requests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {0..10}
-do
-  curl --header "Content-Type: application/json" \
-  --request POST \
-  --data "firstName=john$i&lastName=doe&address=us-east&city=NY&telephone=03131422911" \
-  http://localhost:8080/owners/new
-done
